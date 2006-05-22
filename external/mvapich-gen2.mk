3c40783a70dc23566b60aa59129704684fc9d5a1
Anand V. Avati <avati@80x25.org>
2006-05-22 22:07:09 +0000

    added gpl headers to all source files and makefiles

--- external/mvapich-gen2.mk
+++ external/mvapich-gen2.mk
@@ -1,3 +1,19 @@
+# Copyright (C) 2006 Z RESEARCH Inc. <http://www.zresearch.com>
+#  
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#  
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#  
 mvapich-gen2.%: PACKAGE_NAME=mvapich-gen2-1.0-106
 ifeq ($(ARCH),x86_64)
 mvapich-gen2.%: PKG_CONFIGURE_ENV=$(DEFAULT_PKG_CONFIGURE_ENV) CFLAGS="-D_SMP_ -DLAZY_MEM_UNREGISTER -DRDMA_FAST_PATH -D_X86_64_ -O3 -fno-strict-aliasing -I$(DESTDIR_PKG)/opt/gluster/include" CCFLAGS="$CFLAGS -Wno-deprecated" FFLAGS="-L$(DESTDIR_PKG)/opt/gluster/lib" FC=gfortran ac_cv_prog_AR=$(CROSS)-ar
