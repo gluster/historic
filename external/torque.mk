4b1ad33f8627b7c9638146cf57ceca867e9fcbe9
Anand V. Avati <avati@80x25.org>
2006-05-22 15:23:31 +0000

    torque build environemtn to fix -lncurses

--- external/torque.mk
+++ external/torque.mk
@@ -1,3 +1,4 @@
 torque.%: PACKAGE_NAME=torque-2.0.0p8
 torque.%: PKG_CONFIGURE_CMD=$(PKG_BUILD_ENV) ./configure --prefix=/opt/gluster --set-default-server=master-node --with-scp
+torque.%: PKG_BUILD_CMD=make all $(PKG_CONFIGURE_ENV)
 torque.%: PKG_INSTALL_CMD=make install prefix=$1/opt/gluster; ls torque.setup | cpio -puvd $1/opt/gluster/setup
