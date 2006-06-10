c4d1895163ff3140b4ca8c5bff13bed2c62de166
Anand V. Avati <avati@80x25.org>
2006-06-10 07:02:00 +0000

    fix for #16499 fixes openib build for live mode

--- external/ib-osmtest.mk
+++ external/ib-osmtest.mk
@@ -14,6 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #  
+
 ib-osmtest.%: PACKAGE_NAME=openib-userspace-svn3640-1
 ib-osmtest.%: subdir=osm/osmtest
 ib-osmtest.%: LIVE_PREPARE_CMD=cd userspace/management/$(subdir) \
@@ -23,7 +24,8 @@ ib-osmtest.%: LIVE_CONFIGURE_ENV=LDFLAGS="-L$(DESTDIR_LIVE)/usr/lib -libcommon" 
 ib-osmtest.%: LIVE_CONFIGURE_CMD=cd userspace/management/$(subdir) \
 		&& ([ -f Makefile ] || \
 		./configure --prefix=/usr \
-		--host=$(CROSS) --build=$(GLUSTER_BUILD))
+		--host=$(CROSS) --build=$(GLUSTER_BUILD) \
+		--libdir=$(DESTDIR_LIVE)/usr/lib) 
 ib-osmtest.%: LIVE_BUILD_CMD=cd userspace/management/$(subdir) \
 		&& $(DEFAULT_LIVE_BUILD_CMD)
 ib-osmtest.%: LIVE_INSTALL_CMD=cd userspace/management/$(subdir) \
@@ -39,7 +41,8 @@ ib-osmtest.%: PKG_CONFIGURE_ENV=LDFLAGS="-L$(DESTDIR_PKG)/opt/gluster/lib" \
 ib-osmtest.%: PKG_CONFIGURE_CMD=cd userspace/management/$(subdir) \
 		&& ([ -f Makefile ] || \
 		./configure --prefix=/opt/gluster  \
-		--host=$(CROSS) --build=$(GLUSTER_BUILD))
+		--host=$(CROSS) --build=$(GLUSTER_BUILD) \
+		--libdir=$(DESTDIR_PKG)/opt/gluster/lib)
 ib-osmtest.%: PKG_BUILD_CMD=cd userspace/management/$(subdir) \
 		&& $(DEFAULT_PKG_BUILD_CMD)
 ib-osmtest.%: PKG_INSTALL_CMD=cd userspace/management/$(subdir) \
