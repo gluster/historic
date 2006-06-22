60729cdec0f5949c6748fcf332c55496f38dfc7b
Amar Tumballi <amar@80x25.org>
2006-06-22 15:22:44 +0000

    fixed build bug with torque

--- external/torque.mk
+++ external/torque.mk
@@ -15,6 +15,6 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #  
 torque.%: PACKAGE_NAME=torque-2.0.0p8
-torque.%: PKG_CONFIGURE_CMD=$(PKG_BUILD_ENV) ./configure --prefix=/opt/gluster --set-default-server=master-node --with-scp
+torque.%: PKG_CONFIGURE_CMD=$(PKG_BUILD_ENV) CFLAGS=-I$(DESTDIR_PKG)/opt/gluster/include LDFLAGS=-L/opt/gluster/lib ./configure --prefix=/opt/gluster --set-default-server=master-node --with-scp
 torque.%: PKG_BUILD_CMD=make all $(PKG_CONFIGURE_ENV)
 torque.%: PKG_INSTALL_CMD=make install prefix=$1/opt/gluster; ls torque.setup | cpio -puvd $1/opt/gluster/setup
