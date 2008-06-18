647e8e8b7739b09cc71e459088ec784f7c38f737
Amar Tumballi <amar@80x25.org>
2008-06-18 05:36:13 +0000

    * fixes the issue with xlator.c notify.

--- libglusterfs/src/xlator.c
+++ libglusterfs/src/xlator.c
@@ -270,7 +270,7 @@ xlator_tree_init (xlator_t *xl)
 
   ret = xlator_init_rec (top);
 
-  if (ret == 0 && top->notify) {
+  if (ret == 0 && top->notify && top->parents) {
     top->notify (top, GF_EVENT_PARENT_UP, top->parents->xlator);
   }
 
