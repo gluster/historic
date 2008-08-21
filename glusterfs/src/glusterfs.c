9f2e8aba5074d7d9775c5a5dbbafd7860d2c90be
Amar Tumballi <amar@80x25.org>
2008-08-21 10:40:01 +0000

    * Fixes the logging related to specfile parsing. * All logs related to specfile parsing changed to userfriendly msg (in error case). * using lineno in logging error. * Some changes in parsing way of the same grammer.

--- glusterfs/src/glusterfs.c
+++ glusterfs/src/glusterfs.c
@@ -592,7 +592,7 @@ main (int32_t argc, char *argv[])
 
   graph = xlator_graph_get (ctx, specfp);
   if (!graph) {
-    gf_log ("glusterfs", GF_LOG_ERROR, "Unable to get xlator graph");
+    gf_log ("glusterfs", GF_LOG_DEBUG, "Unable to get xlator graph");
     return -1;
   }
   fclose (specfp);
@@ -613,7 +613,7 @@ main (int32_t argc, char *argv[])
   ctx->graph = graph;
   if (xlator_graph_init (graph) == -1) 
     {
-      gf_log ("glusterfs", GF_LOG_ERROR, 
+      gf_log ("glusterfs", GF_LOG_DEBUG, 
 	      "Error while initializing translators. Exiting");
       if (ctx->mount_point) 
 	{
@@ -649,7 +649,7 @@ main (int32_t argc, char *argv[])
       if (!server_specified)
 	{
 	  gf_log ("glusterfs", GF_LOG_ERROR, 
-		  "Either protocol/volume should be defined in volume spec file, or mountpoint should be given");
+		  "either protocol/volume should be defined in volume spec file, or mountpoint should be given");
 	  return -1;
 	}
     }
