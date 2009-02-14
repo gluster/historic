dcff5e4b02c972840cbf7047c69cbb90658f6935
Anand V. Avati <avati@80x25.org>
2009-02-14 02:48:01 +0000

    fixes in afr and xattr-cache - use local->op instead of transaction type to decide whether post/pre op is needed - few fixes in xattr-cache.c (posix) which was otherwise segfaulting - flush all xattr cache in flush()

--- xlators/storage/posix/src/posix.c
+++ xlators/storage/posix/src/posix.c
@@ -1777,7 +1777,10 @@ posix_flush (call_frame_t *frame, xlator_t *this,
 	pfd = (struct posix_fd *)(long)tmp_pfd;
 
         _fd = pfd->fd;
+
         /* do nothing */
+	posix_xattr_cache_flush_all (this);
+
         op_ret = 0;
 
  out:
